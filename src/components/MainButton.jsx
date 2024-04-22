fd2dce0a18d4451303b8c18d9d7d879ffc34169c
javadEsl <javades755@gmail.com>
2024-04-22 16:21:04 +0330

    Fixed: firs config and change warning color

--- src/components/MainButton.jsx
+++ src/components/MainButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Button, Typography} from "@mui/material";
+import {PaletteLightWarningLight} from "../build/js/tokens";
 
 const MainButton = ({title}) => {
     return (
-        <Button >
+        <Button sx={{backgroundColor:PaletteLightWarningLight}}>
             <Typography>{title}</Typography>
         </Button>
     );
